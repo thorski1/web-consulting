b8eb675220d187ba11a12b57eaf86eeee633e6cd
thorski1 <sthoyre@brightcove.com>
2024-07-16 22:19:01 -0400

    Update: add Segment connection

--- app/(site)/components/global/Analytics/index.tsx
+++ app/(site)/components/global/Analytics/index.tsx
@@ -3,17 +3,15 @@
 import { useEffect } from "react";
 import {
 	usePathname,
-	useSearchParams,
 } from "next/navigation";
 import { analytics } from "@/lib/segment";
 
 export default function Analytics() {
 	const pathname = usePathname();
-	const searchParams = useSearchParams();
 
 	useEffect(() => {
 		analytics.page();
-	}, [pathname, searchParams]);
+	}, [pathname]);
 
 	return null;
 }
