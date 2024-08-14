f76b518ab4d1ae28e7af3b1b1b234ffbd254b1fd
thorski1 <sthoyre@gmail.com>
2024-08-14 16:25:58 -0400

    sync

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -7,8 +7,8 @@ export default function ContactPage() {
 				<div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
 					<div className="space-y-3">
 						<h1 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-							Let's Talk: Your AI-Powered Transformation
-							Starts Here
+							Let&apos;s Talk: Your AI-Powered
+							Transformation Starts Here
 						</h1>
 						<p className="mt-4 text-muted-foreground md:text-xl">
 							Connect with our team to learn how our
